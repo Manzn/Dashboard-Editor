f53a534d31a5dbb27bcac0e703b99cbb50e2414f
Manuel Wagner <manuelj.wagner@gmail.com>
2020-12-04 07:52:23 +0100

    setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import globalStyles from '../styles/index.style';
+
+export default function Home() {
+  return (
+    <div>
+      Seas
+      <style jsx global>
+        {globalStyles}
+      </style>
+    </div>
+  )
+}
